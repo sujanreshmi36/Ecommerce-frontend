23bc2413331268107b88f915ae6083c7f1fe112a
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-03-16 17:12:21 +0545

    upto navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
+    <App />
   </BrowserRouter>
 );
